8014635f961d75cbcbb0de63102bf6b6cd455fe6
iMaxinq <maximosfrou@gmail.com>
2026-06-27 18:30:22 +0300

    granularity change

--- imageProcessing.py
+++ imageProcessing.py
@@ -14,7 +14,7 @@ def denoise(image):
     # High-Granularity Compute: Non-Local Means Denoising
     denoised = cv2.fastNlMeansDenoisingColored(
         image, None, h=10, hColor=10,
-        templateWindowSize=4, searchWindowSize=9
+        templateWindowSize=7, searchWindowSize=20
     )
 
     # Medium Compute: Sharpening Filter
